214d3bce07a00ea718ffce53ca184f9ae899dffb
mattRedbox <matt@redboxresearchdata.com.au>
2014-08-07 17:15:16 +1000

    removed need for add user.

--- manifests/add_directory.pp
+++ manifests/add_directory.pp
@@ -4,10 +4,9 @@ define puppet_common::add_directory ($end_path = $title, $owner = 'puppet', $par
   create_parent_directories($full_path)
 
   file { ["${parent_directory}", "${full_path}"]:
-    ensure  => directory,
-    owner   => $owner,
-    group   => $owner,
-    mode    => $mode,
-    require => Puppet_common::Add_systemuser[$owner],
+    ensure => directory,
+    owner  => $owner,
+    group  => $owner,
+    mode   => $mode,
   }
 }

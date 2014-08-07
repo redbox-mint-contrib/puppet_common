5cfbb82eaf1083061b5dec56051b1c6429c6007e
mattRedbox <matt@redboxresearchdata.com.au>
2014-08-07 17:12:02 +1000

    refactored init_puppet to accommodate hiera.

--- manifests/init_hiera.pp
+++
@@ -1,8 +0,0 @@
-class puppet_common::init_hiera ($puppet_conf_dir = '/etc/puppet',) {
-  puppet_common::add_directory { "hiera_data": parent_directory => $puppet_conf_dir, }
-
-  file { "${puppet_conf_dir}/hiera.yaml":
-    ensure => file,
-    source => "puppet:///modules/puppet_common/hiera.yaml",
-  }
-}
--- manifests/init_puppet.pp
+++ manifests/init_puppet.pp
@@ -1,21 +1,22 @@
 class puppet_common::init_puppet (
-  $conf_file  = "puppet.conf",
-  $home_dir   = "/var/lib/puppet",
-  $system_dir = "/etc/puppet",) {
+  $conf_file = "puppet.conf",
+  $home_dir  = "/var/lib/puppet",
+  $conf_dir  = "/etc/puppet",) {
   # set up puppet configuration file
-  puppet_common::add_directory { "puppet":
-    owner            => "puppet",
-    parent_directory => "/etc"
-  } ->
-  file { "${system_dir}/${conf_file}":
+
+  puppet_common::add_directory { "hiera_data": parent_directory => $conf_dir, } ->
+  file { "${$conf_dir}/${conf_file}":
     ensure => file,
     source => "puppet:///modules/puppet_common/${conf_file}",
   } ->
   file { "${home_dir}/.puppet":
     ensure => link,
-    target => "${system_dir}",
-  } -> notify { 'reset puppet.conf': message => "WARNING: You have reset puppet's conf file." }
-
+    target => "${conf_dir}",
+  } -> notify { 'reset puppet.conf': message => "WARNING: You have reset puppet's conf file." } ->
+  file { "${conf_dir}/hiera.yaml":
+    ensure => file,
+    source => "puppet:///modules/puppet_common/hiera.yaml",
+  }
   # set up .bashrc for use with non-login puppet user.
   file { "${home_dir}/.bashrc":
     ensure => file,

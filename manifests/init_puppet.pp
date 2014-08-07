ea90671092d07c31c5b46a9fdc6aabcd24a7ff60
mattRedbox <matt@redboxresearchdata.com.au>
2014-08-07 18:41:55 +1000

    reverting for now to root user for puppet install.

--- manifests/init_puppet.pp
+++ manifests/init_puppet.pp
@@ -1,7 +1,4 @@
-class puppet_common::init_puppet (
-  $conf_file = "puppet.conf",
-  $home_dir  = "/var/lib/puppet",
-  $conf_dir  = "/etc/puppet",) {
+class puppet_common::init_puppet ($conf_file = "puppet.conf", $home_dir = "/root", $conf_dir = "/etc/puppet",) {
   # set up puppet configuration file
 
   puppet_common::add_directory { "hiera_data": parent_directory => $conf_dir, } ->
@@ -9,18 +6,19 @@ class puppet_common::init_puppet (
     ensure => file,
     source => "puppet:///modules/puppet_common/${conf_file}",
   } ->
-  file { "${home_dir}/.puppet":
-    ensure => link,
-    target => "${conf_dir}",
-  } -> notify { 'reset puppet.conf': message => "WARNING: You have reset puppet's conf file." } ->
   file { "${conf_dir}/hiera.yaml":
     ensure => file,
     source => "puppet:///modules/puppet_common/hiera.yaml",
   }
-  # set up .bashrc for use with non-login puppet user.
-  file { "${home_dir}/.bashrc":
+
+  file { "${home_dir}/.ssh_agent":
     ensure => file,
-    source => "puppet:///modules/puppet_common/bashrc",
-  } -> notify { 'reset .bashrc': message => "WARNING: You have reset puppet's .bashrc file." }
+    source => "puppet:///modules/puppet_common/ssh_agent",
+  } -> notify { 'reset .ssh_agent': message => "WARNING: You have reset puppet's .ssh_agent config." } ->
+  file_line { 'agent_to_bashrc':
+    path  => "${home_dir}/.bashrc",
+    line  => "[[ -s ~/.ssh_agent ]] && ~/.ssh_agent",
+    match => "^.*&& ~/.ssh_agent$"
+  }
 
 }

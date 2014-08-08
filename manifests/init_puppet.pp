ba698f58a08f9e1d544370d27ce8a4336c2c8b1c
mattRedbox <matt@redboxresearchdata.com.au>
2014-08-08 11:14:35 +1000

    corrected run syntax for ssh-agent script. corrected match so no duplicate entry in bashrc for root.

--- manifests/init_puppet.pp
+++ manifests/init_puppet.pp
@@ -1,24 +1,25 @@
 class puppet_common::init_puppet ($conf_file = "puppet.conf", $home_dir = "/root", $conf_dir = "/etc/puppet",) {
-  # set up puppet configuration file
-
+  # set up puppet configuration file for 'root' user
   puppet_common::add_directory { "hiera_data": parent_directory => $conf_dir, } ->
   file { "${$conf_dir}/${conf_file}":
     ensure => file,
     source => "puppet:///modules/puppet_common/${conf_file}",
   } ->
+  # set up hiera basics
   file { "${conf_dir}/hiera.yaml":
     ensure => file,
     source => "puppet:///modules/puppet_common/hiera.yaml",
   }
 
+  # set up ssh config so ssh pull down from repos always ready
   file { "${home_dir}/.ssh_agent":
     ensure => file,
     source => "puppet:///modules/puppet_common/ssh_agent",
   } -> notify { 'reset .ssh_agent': message => "WARNING: You have reset puppet's .ssh_agent config." } ->
   file_line { 'agent_to_bashrc':
     path  => "${home_dir}/.bashrc",
-    line  => "[[ -s ~/.ssh_agent ]] && ~/.ssh_agent",
-    match => "^.*&& ~/.ssh_agent$"
+    line  => "[[ -s ~/.ssh_agent ]] && . ~/.ssh_agent",
+    match => "^[^#]*.ssh_agent$"
   }
 
 }

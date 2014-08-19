e7cc5fdf5bbbb616e9d0730b3d2ef58d2dde6bd2
mattRedbox <matt@redboxresearchdata.com.au>
2014-08-19 12:49:32 +1000

    update directory add to cover cases of undefined parent directory passed as parameter or relative path name passed.

--- lib/puppet/parser/functions/create_parent_directories.rb
+++ lib/puppet/parser/functions/create_parent_directories.rb
@@ -2,7 +2,7 @@
 require 'fileutils'
 module Puppet::Parser::Functions
    newfunction(:create_parent_directories) do |args|
-      full_file_path = args[0]
+      full_file_path = args[0].prepend("/").squeeze("/")
       FileUtils.mkdir_p File.dirname(full_file_path)
    end
 end
--- manifests/add_directory.pp
+++ manifests/add_directory.pp
@@ -1,9 +1,11 @@
-define puppet_common::add_directory ($end_path = $title, $owner = 'puppet', $parent_directory = undef, $mode = '0750') {
+define puppet_common::add_directory ($end_path = $title, $owner = 'root', $parent_directory = undef, $mode = '0750') {
   $full_path = "${parent_directory}/${end_path}"
 
   create_parent_directories($full_path)
 
-  file { ["${parent_directory}", "${full_path}"]:
+  $dir_array = delete_undef_values([$parent_directory, $full_path])
+
+  file { $dir_array:
     ensure => directory,
     owner  => $owner,
     group  => $owner,

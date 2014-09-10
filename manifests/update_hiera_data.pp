7f3c8d679aff57972d7ccc9d66390be6fc736731
mattRedbox <matt@redboxresearchdata.com.au>
2014-09-10 20:45:35 +1000

    updated to use refactored update_hiera manifest.

--- manifests/update_hiera_data.pp
+++ manifests/update_hiera_data.pp
@@ -1,16 +1,12 @@
-define puppet_common::update_hiera_data ($yaml_names = $title,) {
+define puppet_common::update_hiera_data ($yaml_name = $title,) {
   class { 'puppet_common::variables::puppet': } ~>
   puppet_common::add_directory { $puppet_common::variables::puppet::hiera_data_dir: }
 
   if ($caller_module_name) {
-    $yaml_names.each |$value| {
-      file { "${puppet_common::variables::puppet::hiera_data_dir}/${value}":
-        ensure    => file,
-        source    => "puppet:///modules/${caller_module_name}/${value}",
-        subscribe => [
-          Class['Puppet_common::Variables::Puppet'],
-          Class['Puppet_common::Init_hiera']],
-      }
+    file { "${puppet_common::variables::puppet::hiera_data_dir}/${yaml_name}":
+      ensure    => file,
+      source    => "puppet:///modules/${caller_module_name}/${yaml_name}",
+      subscribe => [Class['Puppet_common::Variables::Puppet'], Class['Puppet_common::Init_hiera']],
     }
   }
 }

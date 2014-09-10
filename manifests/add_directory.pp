333fc70e3956b400bcb4f706b7c285a42821e4a7
mattRedbox <matt@redboxresearchdata.com.au>
2014-09-10 22:43:11 +1000

    enabled add directory to re-check parent directory if used by common directory adds.

--- manifests/add_directory.pp
+++ manifests/add_directory.pp
@@ -7,10 +7,17 @@ define puppet_common::add_directory (
 
   create_parent_directories($full_path)
 
-  # parent directory is an optional argument
-  $dir_array = delete_undef_values([$parent_directory, $full_path])
+  if ($parent_directory) {
+    file { "${parent_directory} for ${full_path}":
+      path   => $parent_directory,
+      ensure => directory,
+      owner  => $owner,
+      group  => $owner,
+      mode   => $mode,
+    }
+  }
 
-  file { $dir_array:
+  file { "${full_path}":
     ensure => directory,
     owner  => $owner,
     group  => $owner,

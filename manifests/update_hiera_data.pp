ad5975f8e1aa910e91c6b10fd21aea6c56e01fd1
mattRedbox <matt@redboxresearchdata.com.au>
2014-09-16 06:52:28 +1000

    updated to use node variable.

--- manifests/update_hiera_data.pp
+++ manifests/update_hiera_data.pp
@@ -1,10 +1,10 @@
-define puppet_common::update_hiera_data ($yaml_name = $title,) {
+define puppet_common::update_hiera_data ($yaml_name = $title, $node = undef,) {
   include 'puppet_common::variables::puppet'
 
   if ($caller_module_name) {
     file { "${puppet_common::variables::puppet::hiera_data_dir}/${yaml_name}":
       ensure    => file,
-      source    => "puppet:///modules/${caller_module_name}/${yaml_name}",
+      content   => template("${caller_module_name}/${yaml_name}.erb"),
       subscribe => [Class['Puppet_common::Variables::Puppet'], Class['Puppet_common::Init_hiera']],
     }
   }

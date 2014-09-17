1b1e092c80cbf971fdb602cfb92221ee7961653d
mattRedbox <matt@redboxresearchdata.com.au>
2014-09-17 11:20:45 +1000

    updated pre-install to use method calls.

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-usage() {
-	if [ `whoami` != 'root' ]; 
-		then echo "this script must be executed as root" && exit 1;
-	fi
-}
-usage
+echo "This script only needs to be run ONCE to install puppet, ruby and puppet config."
+echo "Press Ctl-C to exit (or any other key to continue)..."
+read $temp
+
+if [ `whoami` != 'root' ];
+        then echo "this script must be executed as root" && exit 1;
+fi
+
+echo "Will attempt installation..."
 
 ## remove existing installation
-yum remove -y ruby facter puppet
+reset() {
+ yum remove -y ruby facter puppet
+}
 
 ## install ruby installer, rvm
-curl -L get.rvm.io | bash -s stable
-/usr/local/rvm/bin/rvm install ruby-1.9.3
-/usr/local/rvm/bin/rvm pkg install zlib
-/usr/local/rvm/bin/rvm reinstall all --force
-[[ -s /usr/local/rvm/scripts/rvm ]] && source /usr/local/rvm/scripts/rvm
+install_ruby() {
+ curl -L get.rvm.io | bash -s stable
+ /usr/local/rvm/bin/rvm install ruby-1.9.3
+ /usr/local/rvm/bin/rvm pkg install zlib
+ /usr/local/rvm/bin/rvm reinstall all --force
+ [[ -s /usr/local/rvm/scripts/rvm ]] && source /usr/local/rvm/scripts/rvm
+ rvm use 1.9.3 --default
+}
 
-## install modules required
-yum install -y augeas-libs augeas-devel compat-readline5 libselinux-ruby git
-rvm use 1.9.3 --default
-gem install ruby-augeas bundler r10k puppet
+## install modules required for puppet/ruby
+install_puppet() {
+ yum install -y augeas-libs augeas-devel compat-readline5 libselinux-ruby
+ gem install ruby-augeas bundler puppet
+}
+
+reset
+install_ruby
+install_puppet

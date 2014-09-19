276dc366259d299aa1188d2528598fa7edb625cc
mattRedbox <matt@redboxresearchdata.com.au>
2014-09-19 11:22:16 +1000

    corrected syntax for template use.

--- manifests/update_hiera_data.pp
+++ manifests/update_hiera_data.pp
@@ -8,7 +8,7 @@ define puppet_common::update_hiera_data ($yaml_name = $title, $content = undef,)
     file { "${puppet_common::variables::puppet::hiera_data_dir}/${yaml_name}":
       ensure    => file,
       content   => $content ? {
-        undef   => template("${caller_module_name}/${yaml_name}"),
+        undef   => template("${caller_module_name}/${yaml_name}.erb"),
         default => $content,
       },
       subscribe => [Class['Puppet_common::Variables::Puppet'], Class['Puppet_common::Init_hiera']],

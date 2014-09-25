441db8aadcf6c0e3134266dd2662a2f48c69f5f2
mattRedbox <matt@redboxresearchdata.com.au>
2014-09-25 17:04:40 +1000

    refactored cron to use create_resources

--- manifests/add_cron.pp
+++ manifests/add_cron.pp
@@ -1,10 +1,4 @@
 define puppet_common::add_cron ($crontab = $title,) {
-  $crontab.each |$key, $value| {
-    cron { $key:
-      command => $value[command],
-      user    => $value[user],
-      hour    => $value[hour],
-      minute  => $value[minute],
-    }
-  }
+  create_resources(cron, $crontab)
+
 }

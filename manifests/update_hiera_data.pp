6e44251f863c8d4d68f01ebfb5fbf0f2e74b0be4
mattRedbox <matt@redboxresearchdata.com.au>
2014-10-01 09:11:50 +1000

    added option to update environment name in main puppet config.

--- manifests/update_hiera_data.pp
+++ manifests/update_hiera_data.pp
@@ -1,4 +1,7 @@
-define puppet_common::update_hiera_data ($yaml_name = $title, $content = undef,) {
+define puppet_common::update_hiera_data (
+  $yaml_name        = $title,
+  $content          = undef,
+  $environment_name = undef) {
   require 'puppet_common::variables::puppet'
 
   ensure_resource('puppet_common::add_directory', 
@@ -12,5 +15,13 @@ define puppet_common::update_hiera_data ($yaml_name = $title, $content = undef,)
     },
     subscribe => Puppet_common::Add_directory["${puppet_common::variables::puppet::hiera_data_dir}"
       ],
+  }
+
+  if ($environment) {
+    file_line { 'set puppet.conf environment':
+      path  => "${puppet_common::variables::puppet::conf_dir}/${puppet_common::variables::puppet::hiera_config_name}",
+      line  => "environment=${environment_name}",
+      match => "^[\\s]*environment[\\s]*=[\\s]*[a-zA-Z]+$"
+    }
   }
 }

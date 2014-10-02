1fcec63bdd568637021ca888e29edde70c764f62
mattRedbox <matt@redboxresearchdata.com.au>
2014-10-03 09:35:47 +1000

    added path for exec.

--- manifests/init_puppet.pp
+++ manifests/init_puppet.pp
@@ -2,8 +2,20 @@ class puppet_common::init_puppet (
   $home_dir                   = "/root",
   $ssh_key                    = undef,
   $environment                = undef,
-  $has_directory_environments = true,) {
+  $has_directory_environments = true,
+  $exec_path                  = hiera_array(exec_path, [
+    '/usr/local/bin',
+    '/opt/local/bin',
+    '/usr/bin',
+    '/usr/sbin',
+    '/bin',
+    '/sbin']),) {
   host { [$::fqdn]: ip => $::ipaddress, }
+
+  Exec {
+    path      => $exec_path,
+    logoutput => false,
+  }
 
   exec { 'link_ruby_puppet_binary':
     command   => 'ln -s `which puppet` /usr/bin/puppet',

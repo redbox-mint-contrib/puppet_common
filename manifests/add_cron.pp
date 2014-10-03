1572168dbd2f80edf89b4a7666449c207eb67ba6
mattRedbox <matt@redboxresearchdata.com.au>
2014-10-03 10:15:10 +1000

    wrapped environment option in variable.

--- manifests/add_cron.pp
+++ manifests/add_cron.pp
@@ -1,7 +1,11 @@
 define puppet_common::add_cron ($crontab = $title, $cron_path = undef,) {
-  create_resources(cron, $crontab, {
-    environment => "PATH=${cron_path}:$PATH",
+  if ($cron_path) {
+    create_resources(cron, $crontab, {
+      environment => "PATH=${cron_path}:$PATH",
+    }
+    )
+  } else {
+    create_resources(cron, $crontab)
   }
-  )
 
 }

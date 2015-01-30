e5732e7cd20e62a68223f170ec18145a598e66cd
mattRedbox <matt@redboxresearchdata.com.au>
2015-01-30 21:07:22 +1000

    corrected syntax.

--- manifests/add_gpg_from_repo.pp
+++ manifests/add_gpg_from_repo.pp
@@ -2,7 +2,7 @@ define puppet_common::add_gpg_from_repo (
   $key_name     = hiera_hash('gpg_key::name', $title),
   $source       = hiera_hash('gpg_key::source', undef),
   $path         = hiera_hash('gpg_key::path', undef),
-  $gpg_home_dir = hiera_hash('gpg::home_dir', "${::settings::confdir}/gpg",) {
+  $gpg_home_dir = hiera_hash('gpg::home_dir', "${::settings::confdir}/gpg"),) {
   ensure(file, $gpg_home_dir, {
     ensure  => directory,
     owner   => $puppet_user,
